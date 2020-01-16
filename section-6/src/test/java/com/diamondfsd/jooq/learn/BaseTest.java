6d5e2ccf40cb71b32f5eacad041aee8541a91736
diamondfsd <diamondfsd@gmail.com>
2020-01-16 13:51:40 +0800

    remove unused base test

--- section-6/src/test/java/com/diamondfsd/jooq/learn/BaseTest.java
+++ section-6/src/test/java/com/diamondfsd/jooq/learn/BaseTest.java
@@ -1,8 +1,6 @@
 package com.diamondfsd.jooq.learn;
 
 import org.jooq.DSLContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.transaction.annotation.Transactional;
