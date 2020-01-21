98ac190c17bcc749d4d41e14d9be32785c4f8fbf
diamondfsd <diamondfsd@gmail.com>
2020-01-21 17:39:23 +0800

    remove some unused code

--- section-10/src/main/java/com/diamondfsd/jooq/learn/Section10Main.java
+++ section-10/src/main/java/com/diamondfsd/jooq/learn/Section10Main.java
@@ -1,9 +1,7 @@
 package com.diamondfsd.jooq.learn;
 
-import com.diamondfsd.jooq.learn.jooq.tables.daos.S1UserDao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @author Diamond
@@ -11,8 +9,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class Section10Main {
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(Section10Main.class);
-        S1UserDao bean = run.getBean(S1UserDao.class);
-        bean.update();
+        SpringApplication.run(Section10Main.class);
     }
 }
