ae9464527c9af2a8fe68e60c85e2a0cc8f904ac0
diamondfsd <diamondfsd@gmail.com>
2020-01-22 15:03:14 +0800

    change custom pojo

--- section-10/src/main/java/com/diamondfsd/jooq/learn/pojos/S2UserMessage.java
+++ section-10/src/main/java/com/diamondfsd/jooq/learn/pojos/S2UserMessage.java
@@ -5,5 +5,13 @@ import com.diamondfsd.jooq.learn.jooq.tables.pojos.S2UserMessagePojo;
 
 
 public class S2UserMessage extends S2UserMessagePojo {
+    String username;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
---
+++ section-10/src/test/java/com/diamondfsd/jooq/learn/pojos/S2UserMessageTest.java
@@ -0,0 +1,42 @@
+package com.diamondfsd.jooq.learn.pojos;
+
+import com.diamondfsd.jooq.learn.jooq.tables.daos.S2UserMessageDao;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static com.diamondfsd.jooq.learn.jooq.Tables.S1_USER;
+import static com.diamondfsd.jooq.learn.jooq.Tables.S2_USER_MESSAGE;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback
+class S2UserMessageTest {
+
+    @Autowired
+    S2UserMessageDao userMessageDao;
+
+    @Test
+    void getUsername() {
+        List<S2UserMessage> userMessageList = userMessageDao.create()
+                .select(S2_USER_MESSAGE.ID, S2_USER_MESSAGE.MESSAGE_TITLE, S1_USER.USERNAME)
+                .from(S2_USER_MESSAGE)
+                .leftJoin(S1_USER).on(S1_USER.ID.eq(S2_USER_MESSAGE.USER_ID))
+                .fetchInto(S2UserMessage.class);
+
+        Assertions.assertTrue(userMessageList.size() > 0);
+        List<String> userNames = userMessageList.stream().map(S2UserMessage::getUsername)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+        Assertions.assertEquals(userMessageList.size(), userNames.size());
+    }
+
+}
